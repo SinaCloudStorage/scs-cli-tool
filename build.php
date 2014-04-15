1d3150d5d1f3fd4cab21da383160fe384dd5509f
Bruce Chen <662005@qq.com>
2014-04-15 22:44:26 +0800

    nothing

---
+++ build.php
@@ -0,0 +1,12 @@
+<?php
+$phar = new Phar('bin/scs.phar');
+$phar->setAlias('scs.phar');
+$phar->setStub('#!/usr/bin/env php
+<?php Phar::mapPhar(); 
+include "phar://scs.phar/bootstrap.php";
+__HALT_COMPILER(); ?>');
+$phar->buildFromDirectory(dirname(__FILE__) . '/src' );
+$phar->compressFiles(Phar::GZ);
+$phar->stopBuffering();
+
+?>
